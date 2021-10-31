736415a28c0b225af1f084cf0bc0cc7e1225f52a
Matias Maravi <91230547+MatiasMaravi@users.noreply.github.com>
2021-10-31 02:48:48 +0000

    funciones documentadas correctamente

--- funcion.py
+++ funcion.py
@@ -4,40 +4,61 @@ import platform  #Funciones para averiguar cosas sobre la plataforma
 
 #Aqui van sus funciones no olviden luego yo lo ordeno en el main
 
-#Esta funcion limpia la consola
+
 def limpiar_consola(): 
-    if platform.system() == "Windows": #valida en que plataforma estas 
-        os.system("cls")
-    else:
-        os.system("clear")
+  """
+  Esta funcion limpia la consola dependiendo 
+  que sistema operativo estes usando
+  """
+  if platform.system() == "Windows": 
+      os.system("cls")
+  else:
+      os.system("clear")
 
-#Esta funcion permite ingresar letra por letra para adivinar la palabra
-def jugar(adivinar: str): #El parametro es la palabra a adivinar
-    adivinar = adivinar.upper() #El parametro sera todo mayusculas
-    palabra = "" #En un comienzo la palabra estara vacia
-    while True:
-        guiones = 0 #Contador de los guiones que hay
-        for letra in adivinar: #Para cada letra de la frase o palabra a adivninar
-            if letra in palabra: #Si la letra de adivinar coincide con  la letra que escribimos se imprime
-                print(letra,end=" ")
-            elif letra == " ":
-                print("  ",end=" ") #Si es una frase el espacio no sera un guion
-            else:
-                print("_",end=" ") #Si la letra no esta escribe un guion en su lugar
-                guiones +=1 
-        if guiones == 0: #Si todo es letra y no hay guiones ganas el juego
-            print("") 
-            print("Felicidades ganaste")
-            break
-        print("")
-        tu_letra= input("Introduce tu letra: ").upper()
 
-        if len(tu_letra) > 1:
-            print("Por favor solo una letra a la vez")
-        else:    
-            palabra+=tu_letra
+def jugar(adivinar): 
+  """
+  Esta funcion recibe como parametro una palabra
+  en mayusculas y comenzara a iterar letra por letra
+  de la palabra a advinar buscando similitudes con las letras que 
+  vayamos ingresando, si no encuentra letras coincidentes
+  imprimira un guion bajo en su lugar.
+  
+  Parámetros:
+    adivinar -- palabra o frase que se utilizara para iterar(Solo
+    pueden ser letras) 
 
-'''
-#Esta funcion permite que el juego termine despues de los 6 intetos.
-def intentos(intentos):
-'''
+  Si al iterar no encuentra guiones bajos la funcion terminara.
+  
+  Si el parametro a adivinar es una frase, el "espacio" no se
+  considerara como un guion.
+
+  Solo permite ingresar una letra a la vez para comparar
+  """
+  adivinar = adivinar.upper() 
+  palabra = "" 
+  while True:
+      guiones = 0
+      for letra in adivinar: 
+          if letra in palabra: 
+              print(letra,end=" ")
+          elif letra == " ":
+              print("  ",end=" ") 
+          else:
+              print("_",end=" ") 
+              guiones +=1 
+      if guiones == 0: 
+          print("") 
+          print("Felicidades ganaste")
+          break
+      print("")
+      tu_letra= input("Introduce tu letra: ").upper()
+
+      if len(tu_letra) > 1:
+          print("Por favor solo una letra a la vez")
+      else:    
+          palabra+=tu_letra
+
+
+#Esta funcion permite que el juego termine despues de los 6 intentos.
+def intentos(errores):
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import funcion
 import categoria
 
-funcion.jugar("perro")
+
 
 
 
